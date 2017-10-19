1336f78b258781f790c084d31bea191d294e8d71
Larissa Ho <holarissa12@gmail.com>
2017-10-19 14:47:53 -0700

    Add exercise answers

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,31 +1,31 @@
 # Exercise 4: DPLYR and flights data
 
 # Install the nycflights13 package and read it in.  Require the dplyr package
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
 
 # Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+flights$time.gained <- flights$arr_delay - flights$dep_delay
 
 # Sort your data.frame desceding by the column you just created
-
+flights <- arrange(flights, desc(time.gained))
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- flights %>% mutate(gain = arr_delay - dep_delay) %>% arrange(flights, desc(flights$time.gained))
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$time.gained)
 
 # On average, did flights gain or lose time?
-
+mean(flights$time.gained)
 
 # Create a data.frame that is of flights headed to seatac ('SEA'), 
-
+ 
 
 # On average, did flights to seatac gain or loose time?
 
